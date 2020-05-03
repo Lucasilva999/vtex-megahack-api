bdbe7d9df2dc30fb847d5d7b953ca114714dd8fe
Lucasilva88 <Lucasilva999@hotmail.com.br>
2020-05-03 16:02:47 -0300

    Adição de Schema de pedido do produto

---
+++ src/infra/database/mongoose/models/ProductOrder.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ProductOrderSchema = mongoose.Schema(
+  {
+    productId: {
+      type: [mongoose.Schema.ObjectId],
+      required: true,
+    },
+    productName: {
+      type: [String],
+      required: true,
+    },
+    orderValue: {
+      type: Number,
+      required: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+    },
+    deliveryAddress: {
+      type: String,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["SOLD", "BEING_PACKED", "PREPARING_FOR_DELIVERY", "DELIVERED"],
+      default: "SOLD",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("ProductOrder", ProductOrderSchema);
